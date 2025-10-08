8b02b764e207f7663ea563c4c3da018006142a3d
Sérgio Breda <sergio.breda.prm@gmail.com>
2025-10-08 00:02:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9363135755-c7f98",
+  "appId": "1:815910917636:web:08ec0b108296e7521bc751",
+  "apiKey": "AIzaSyBpoOMLqHS2pI1-SHHLaTdli4t2yZ8TcsE",
+  "authDomain": "studio-9363135755-c7f98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "815910917636"
+};
